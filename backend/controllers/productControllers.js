77d638f8546813584d2221a2636f2febdb1ad5a1
Andres Lopez Esquivel <lopezesquivelandres1@gmail.com>
2026-02-20 23:42:09 -0600

    Implement GET products controller

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,5 +1,18 @@
-function getProducts(req, res) {
-  res.send("Hello from getProducts");
+import { sql } from "../config/db.js";
+
+async function getProducts(req, res) {
+  try {
+    const products = await sql`
+      SELECT * FROM products
+    `;
+    console.log(`typeof products: ${ typeof products }`);
+    console.log("Products: ", products);
+    res.status(200).json(products); // https://expressjs.com/en/api.html#res.json
+  } catch (e) {
+    console.log('Error retrieving products. ').
+    console.log(e);
+    res.status(500).json({ success : false, message: "Error retrieving products" });
+  }
 }
 
 function getProduct(req, res) {
@@ -7,7 +20,9 @@ function getProduct(req, res) {
 }
 
 function createProduct(req, res) {
-  res.send("Hello from createProduct");
+  // To use this middle, we need to previously have
+  // set app.use(express.json());
+
 }
 
 function updateProduct(req, res) {
