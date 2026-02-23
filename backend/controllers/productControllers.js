001e2527a5e3a18d2b94401d1709ff33a6790c25
Andres Lopez Esquivel <lopezesquivelandres1@gmail.com>
2026-02-23 16:50:35 -0600

    Implement product creation controller, using .then instead of await

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -16,10 +16,10 @@ async function getProducts(req, res) {
 }
 
 async function getProduct(req, res) {
-  const product_id = req.params.id;
+  const productId = req.params.id;
   try {
     const product = await sql`
-      SELECT * FROM products WHERE id = ${product_id};
+      SELECT * FROM products WHERE id = ${productId};
     `;
     console.log(`typeof product = ${ typeof product }`);
     console.log(product);
@@ -32,9 +32,33 @@ async function getProduct(req, res) {
 }
 
 function createProduct(req, res) {
-  // To use this middle, we need to previously have
-  // set app.use(express.json());
-
+  const { name, image, price } = req.body;
+  console.log(req.body);
+  console.log(`name: ${name}`);
+  console.log(`image: ${image}`);
+  console.log(`price: ${price}`);
+  try {
+    // Here I could have used await but I prefered to try another approach
+    // to test my knowledge on Promises handling
+    sql`
+      INSERT INTO products
+      (name, image, price)
+      VALUES
+      (${name}, ${image}, ${price})
+      RETURNING *
+    `.then(newProduct => {
+      console.log(`typeof product = ${ typeof newProduct }`);
+      console.log(newProduct);
+      res.status(200).json(newProduct[0]);
+    });
+  } catch (e) {
+    console.log("An error occurred while creating a new product.");
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "An error occurred while creating a new product."
+    });
+  }
 }
 
 function updateProduct(req, res) {
