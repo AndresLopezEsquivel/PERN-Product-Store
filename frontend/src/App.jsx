fe1fe9b87f53a2f259c40f96dc9e30ef7b3c4ba2
Andres Lopez Esquivel <lopezesquivelandres1@gmail.com>
2026-02-27 19:50:21 -0600

    Set up components and pages files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,20 @@
+// Components
+import Navbar from "./components/Navbar.jsx";
+// Pages
+import HomePage from "./pages/HomePage.jsx";
+import ProductPage from "./pages/ProductPage.jsx";
+// Packages
+import { Routes, Route } from "react-router-dom";
+
 function App() {
 	return(
-		<div>
-			<h1 className="text-red-500"> Hey! </h1>
+		<div className="min-h-screen bg-base-200 transition-colors duration-300">
+      { /* We'd like to see the navbar on every single page, that's why it is not in Routes */ }
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product/:id" element={<ProductPage />} />
+      </Routes>
 		</div>
 	);
 }
